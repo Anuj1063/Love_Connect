bb8977ced64045385615caa00d6ba4c802f3117d
Anuj1063 <anujkumar11063@gmail.com>
2025-07-10 00:40:21 +0530

    add slace

--- client/src/utils/basUrl.jsx
+++ client/src/utils/basUrl.jsx
@@ -1,5 +1,5 @@
 const BaseUrl = import.meta.env.MODE === "production"
-  ? "https://love-connect-1.onrender.com"
-  : "http://localhost:4000";
+  ? "https://love-connect-1.onrender.com/"
+  : "http://localhost:4000/";
 
 export default BaseUrl;
