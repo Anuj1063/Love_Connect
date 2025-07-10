51f24349b6cd7bff787356d64e975f0037ef1f66
Anuj1063 <anujkumar11063@gmail.com>
2025-07-10 10:40:14 +0530

    location set

--- server/server.js
+++ server/server.js
@@ -1,40 +1,51 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const cors = require("cors");
 
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const cors = require('cors');
- 
-const { app, server, initSocketIO } = require('./app/utils/socket.util');
-const connectDB = require('./app/config/db');
-
+const { app, server, initSocketIO } = require("./app/utils/socket.util");
+const connectDB = require("./app/config/db");
 
 // CORS Middleware
-if(process.env.NODE_ENV!=='production'){
-
+if (process.env.NODE_ENV !== "production") {
 }
 const corsMiddleware = cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173" ,
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
-}); 
+});
 
 // Static Files with CORS
-app.use("/uploads/profile", corsMiddleware, express.static(path.join(__dirname, "uploads/profile")));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(
+  "/uploads/profile",
+  corsMiddleware,
+  express.static(path.join(__dirname, "uploads/profile"))
+);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Middlewares
 app.use(corsMiddleware);
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "https://js.stripe.com"],
-        connectSrc: ["'self'", "http://localhost:4000", "https://love-connect-1.onrender.com"],
-        imgSrc: ["'self'", "data:", "https://images.unsplash.com","https://plus.unsplash.com"],
+        connectSrc: [
+          "'self'",
+          "http://localhost:4000",
+          "https://love-connect-1.onrender.com",
+          "https://nominatim.openstreetmap.org",
+        ],
+        imgSrc: [
+          "'self'",
+          "data:",
+          "https://images.unsplash.com",
+          "https://plus.unsplash.com",
+        ],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com"],
         frameSrc: ["'self'", "https://js.stripe.com"], // for Stripe
@@ -46,20 +57,19 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // socket Connection
 initSocketIO();
 
 // Routes
-app.use('/auth', require('./app/routes/auth/auth.route'));
-app.use('/api/profile', require('./app/routes/api/profile.route')); 
-app.use('/api/swipe', require('./app/routes/api/swipe.route'));
-app.use('/api/match', require('./app/routes/api/match.route'));
-app.use('/api', require('./app/routes/api/preference.route'));
-app.use('/api/message', require('./app/routes/api/message.route'));
-app.use("/api/admin", require('./app/routes/api/admin.route'))
-app.use("/api/payment", require('./app/routes/api/payment.route'));
-app.use("/api", require("./app/routes/api/comments.route"))
+app.use("/auth", require("./app/routes/auth/auth.route"));
+app.use("/api/profile", require("./app/routes/api/profile.route"));
+app.use("/api/swipe", require("./app/routes/api/swipe.route"));
+app.use("/api/match", require("./app/routes/api/match.route"));
+app.use("/api", require("./app/routes/api/preference.route"));
+app.use("/api/message", require("./app/routes/api/message.route"));
+app.use("/api/admin", require("./app/routes/api/admin.route"));
+app.use("/api/payment", require("./app/routes/api/payment.route"));
+app.use("/api", require("./app/routes/api/comments.route"));
 
 if (process.env.NODE_ENV === "production") {
   const clientBuildPath = path.join(__dirname, "../client/dist");
@@ -67,13 +77,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(clientBuildPath));
 
   // Catch-all route for React Router
- app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(clientBuildPath, "index.html"));
-});
-
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
+  });
 }
-
-
 
 // DB Connection
 connectDB.dbConnection();
